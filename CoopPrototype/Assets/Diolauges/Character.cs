552a13a8726637d12126a110036fa92a4889a931
Ianmring <Ianmring@gmail.com>
2019-05-25 23:17:02 -0700

    Convosystemdone

---
+++ CoopPrototype/Assets/Diolauges/Character.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Diolauge/Characters")]
+
+public class Character : ScriptableObject
+{
+
+
+    public string Name;
+    public Sprite[] expressions;
+
+  
+}
