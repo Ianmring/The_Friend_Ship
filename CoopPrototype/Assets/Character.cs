932d81b3083fb94ab4941989b13654e6544d0426
Ianmring <Ianmring@gmail.com>
2019-05-27 23:55:47 -0700

    Final commit for Coolprototype

---
+++ CoopPrototype/Assets/Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Diolauge/Characters")]
+public class Character : ScriptableObject
+{
+
+    public string Name;
+    public Sprite[] expressions;
+
+
+}
---
+++ CoopPrototype/Assets/Diolauge.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Diolauge/Diolauge")]
+public class Diolauge : ScriptableObject
+{
+    // Start is called before the first frame update
+
+    //public string convoname;
+    public enum Expression { happy, sad, talking, confused, angry, laughing, surprised, silly };
+
+
+
+    public string name;
+
+    [TextArea(3, 4)]
+    public string[] Sentences;
+    public Character[] Character_in_Conversation;
+    public Expression[] currentexpressions;
+
+
+ //   public Sentence[] convo;
+}
