6eefb9298ee442e72d2d9b6e783a405172c8bfb4
Ianmring <Ianmring@gmail.com>
2019-05-28 23:03:10 -0700

    Diolauge system
    re integrated

---
+++ The_Friend_Ship_Demo/Assets/Diolauge/DiolaugeTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiolaugeTrigger : MonoBehaviour
+{
+
+    public Diolauge Dio;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Triggerdio()
+    {
+        FindObjectOfType<DiolaugeManager>().Startdio(Dio);
+    }
+}
